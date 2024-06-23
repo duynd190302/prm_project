5380e1fd6448e00a016f9dd9a9cdbc8a95830a0e
duynd190302 <duynd190302@gmail.com>
2024-06-23 13:08:29 +0700

    xu ly dang ki va dang nhap

--- app/src/main/java/com/example/appbanhang/activity/DangkiActivity.java
+++ app/src/main/java/com/example/appbanhang/activity/DangkiActivity.java
@@ -1,20 +1,109 @@
 package com.example.appbanhang.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.appbanhang.R;
+import com.example.appbanhang.retrofit.ApiBanHang;
+import com.example.appbanhang.retrofit.RetrofitClient;
+import com.example.appbanhang.utils.Utils;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class DangkiActivity extends AppCompatActivity {
-
+    EditText email, pass, repass, mobile, username;
+    AppCompatButton button;
+    ApiBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ki);
+        initView();
+        initControl();
+    }
+
+    private void initControl() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dangKi();
+            }
+        });
+    }
+
+    private void dangKi() {
+        String str_email = email.getText().toString().trim();
+        String str_pass = pass.getText().toString().trim();
+        String str_repass = repass.getText().toString().trim();
+        String str_mobile = mobile.getText().toString().trim();
+        String str_username = username.getText().toString().trim();
+        if(TextUtils.isEmpty(str_email)){
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập Email", Toast.LENGTH_LONG).show();
+        }else if (TextUtils.isEmpty(str_username)) {
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập Username", Toast.LENGTH_LONG).show();
+        }else if (TextUtils.isEmpty(str_pass)) {
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập Pass", Toast.LENGTH_LONG).show();
+        }else if (TextUtils.isEmpty(str_repass)) {
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập Repass", Toast.LENGTH_LONG).show();
+        }else if (TextUtils.isEmpty(str_mobile)) {
+            Toast.makeText(getApplicationContext(),"Bạn chưa nhập Mobile", Toast.LENGTH_LONG).show();
+        }else {
+            if(str_pass.equals(str_repass)){
+                compositeDisposable.add(apiBanHang.dangKi(str_email,str_pass,str_username,str_mobile)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                userModel -> {
+                                    if(userModel.isSuccess()){
+                                        Utils.user_current.setEmail(str_email);
+                                        Utils.user_current.setEmail(str_pass);
+                                        Intent intent = new Intent(getApplicationContext(), DangnhapActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else {
+                                        Toast.makeText(getApplicationContext(),userModel.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                },
+                                throwable -> {
+                                    Toast.makeText(getApplicationContext(),throwable.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                        )
+                );
+            }else {
+                Toast.makeText(getApplicationContext(), "Repass chưa khớp", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void initView() {
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        repass = findViewById(R.id.repass);
+        mobile= findViewById(R.id.mobile);
+        username = findViewById(R.id.username);
+        button = findViewById(R.id.btndangky);
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/appbanhang/retrofit/ApiBanHang.java
+++ app/src/main/java/com/example/appbanhang/retrofit/ApiBanHang.java
@@ -2,6 +2,7 @@ package com.example.appbanhang.retrofit;
 
 import com.example.appbanhang.model.LoaiSpModel;
 import com.example.appbanhang.model.SanPhamMoiModel;
+import com.example.appbanhang.model.UserModel;
 
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.Field;
@@ -10,15 +11,32 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiBanHang {
+    //Get data
     @GET("getloaisp.php")
     Observable<LoaiSpModel> getLoaisp();
     @GET("getspmoi.php")
     Observable<SanPhamMoiModel> getSpMoi();
+    //Post data
     @POST("chitiet.php")
     @FormUrlEncoded
     Observable<SanPhamMoiModel> getSanPham(
       @Field("page") int page,
       @Field("loai") int loai
+    );
+    @POST("dangki.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangKi(
+      @Field("email") String email,
+      @Field("pass") String pass,
+      @Field("username") String username,
+      @Field("mobile") String mobile
+    );
+    @POST("dangnhap.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangNhap(
+            @Field("email") String email,
+            @Field("pass") String pass
+
     );
 
 }
